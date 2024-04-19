f9162499a16590244b76e80406601aebf7ade177
ikubay <ikubaysan@gmail.com>
2024-04-18 19:50:58 -0700

    Fixed color 1 not being set

--- ResoniteGBApp/ResoniteGBApp/FrameData.cs
+++ ResoniteGBApp/ResoniteGBApp/FrameData.cs
@@ -50,7 +50,7 @@ namespace ResoniteGBApp
         private static int GetGameBoyColorIndex(Color originalColor)
         {
             int brightness = (int)(0.299 * originalColor.R + 0.587 * originalColor.G + 0.114 * originalColor.B);
-            if (brightness >= 192) 
+            if (brightness > 192) 
                 return 0;  // White
             if (brightness >= 128) 
                 return 1;  // Light Gray
